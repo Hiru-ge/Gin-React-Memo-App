a4c115e2c7bf45d2f6fabba57d95d0c8ef8c7f46
Hiru-ge <okita0249@gmail.com>
2025-12-18 21:10:52 +0900

    [update] DBとの接続を確認

--- backend-API/main.go
+++ backend-API/main.go
@@ -1,8 +1,39 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
 
 func main() {
+	cfg := mysql.NewConfig()
+	cfg.User = os.Getenv("DBUSER")
+	cfg.Passwd = os.Getenv("DBPASS")
+	cfg.Net = "tcp"
+	cfg.Addr = "127.0.0.1:3306"
+	cfg.DBName = "memo_app"
+
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// DB接続確認
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Println("Connected!")
+
+	// ルーティング
 	router := gin.Default()
 
 	router.Run("localhost:8080")
