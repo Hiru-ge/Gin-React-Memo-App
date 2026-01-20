4190f59ab4487f7941eff41be9b40121219965d9
Hiru-ge <okita0249@gmail.com>
2026-01-20 22:15:04 +0900

    [move clean] フロントエンドのファイル名をケバブケースに統一
    React準拠でキャメルケースにするか、ReactRouter準拠でケバブケースにするか迷ったが後者にした

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -3,7 +3,7 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
     index("routes/index.tsx"),
     route("memos/:id", "routes/memo.tsx"),
-    route("memos/:id/edit", "routes/edit_memo.tsx"),
-    route("memos/new", "routes/new_memo.tsx"),
-    route("memos/delete", "routes/delete_memo.tsx"),
+    route("memos/:id/edit", "routes/edit-memo.tsx"),
+    route("memos/new", "routes/new-memo.tsx"),
+    route("memos/delete", "routes/delete-memo.tsx"),
 ] satisfies RouteConfig;
